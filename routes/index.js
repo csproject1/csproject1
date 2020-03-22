c4d990ba79014b99031463a3cc08864ab649fb93
Prathmesh Pathak <34978373+prarthamjp786@users.noreply.github.com>
2020-03-22 14:38:11 -0400

    New Repo for class

---
+++ routes/index.js
@@ -0,0 +1,129 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const fs = require('fs');
+
+const initializePassport = require('../passport-config');
+initializePassport(
+    passport,
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+);
+
+const users = [];
+const dog = [];
+
+router.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', { name: req.user.firstName });
+})
+
+router.get('/home', checkNotAuthenticated, (req, res) => {
+    res.render('home.ejs');
+});
+
+router.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login.ejs');
+})
+
+router.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+
+router.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register.ejs');
+})
+
+router.post('/register', checkNotAuthenticated, async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        users.push({
+            id: Date.now().toString(),
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            zipcode: req.body.zip,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login');
+    }
+    catch {
+        res.redirect('/register');
+    }
+})
+
+router.delete('/logout', (req, res) => {
+    req.logOut();
+    res.redirect('/login');
+});
+
+router.get('/search-sitter', checkAuthenticated, (req, res) => {
+    fs.readFile('sitter_list.json', (err, data) => {
+        if (err) console.log(err);
+        let sitter = JSON.parse(data);
+        console.log(sitter);
+        res.render('sitter.ejs', {
+            userData: req.user,
+            sitterData: sitter
+        });
+    });
+});
+
+router.get('/tips-tricks', (req, res) => {
+    res.render('dog-care.ejs');
+});
+
+router.get('/profile', (req, res) => {
+    if (typeof dog == "undefined" || dog == null || dog.length == 0) {
+        res.render('profile.ejs', {
+            userData: req.user
+        });
+    }
+    else {
+        res.render('profile-dog.ejs', {
+            userData: req.user,
+            dogData: dog
+        });
+    }
+});
+
+router.get('/profile/add', (req, res) => {
+    res.render('user-dog.ejs');
+});
+
+router.post('/profile/add', checkAuthenticated, (req, res) => {
+    dog.push({
+        id: Date.now().toString(),
+        name: req.body.dogName,
+        weight: req.body.weight,
+        breed: req.body.dogBreed,
+        ageYears: req.body.ageYears,
+        ageMonths: req.body.ageMonths,
+        gender: req.body.gender,
+        cats: req.body.cats,
+        isMicrochipped: req.body.microchipped,
+        nature: req.body.nature,
+        children: req.body.children,
+        isTrained: req.body.trained
+    });
+    console.log(dog);
+    res.redirect('/profile');
+});
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/');
+    }
+    next();
+}
+
+module.exports = router;
